6fab744de54c29b1deecf76356e088510ce9501d
Rodion Mustafev <rodion9116@yandex.ru>
2025-01-29 15:32:57 +1000

    create commandHandler functions and added it to main

--- my-golang-app/cmd/main.go
+++ my-golang-app/cmd/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	Command "my-golang-app/pkg/commandHandler"
 	"os"
+	"strconv"
 )
 
 func main() {
@@ -11,33 +13,46 @@ func main() {
 	switch command {
 	case "add":
 		fmt.Println("Add command")
+		Command.AddTask(os.Args[2])
 
 	case "update":
 		fmt.Println("Update command")
+		taskId, _ := strconv.Atoi((os.Args[2]))
+		Command.UpdateTask(taskId, os.Args[3])
 
 	case "delete":
+		taskId, _ := strconv.Atoi((os.Args[2]))
+		Command.DeleteTask(taskId)
 		fmt.Println("delete command")
 
 	case "list":
 		if !(len(os.Args) < 3) {
 			if os.Args[2] == "done" {
 				fmt.Println("list done")
+				Command.ListDoneTasks()
 			} else if os.Args[2] == "todo" {
 				fmt.Println("List todo")
+				Command.ListTodoTasks()
 			} else if os.Args[2] == "in-progress" {
 				fmt.Println("List in-progress")
+				Command.ListInProgressTasks()
 			} else {
 				fmt.Println("Wrong command. Check tutotial")
 			}
 		} else {
 			fmt.Println("List command")
+			Command.ListTasks()
 		}
 
 	case "mark-in-progress":
 		fmt.Println("Mark-in-progress command")
+		taskId, _ := strconv.Atoi((os.Args[2]))
+		Command.MarkTaskInProgress(taskId)
 
 	case "mark-done":
 		fmt.Println("Mark-done command")
+		taskId, _ := strconv.Atoi((os.Args[2]))
+		Command.MarkTaskDone(taskId)
 
 	default:
 		fmt.Println("Wrong command")
---
+++ my-golang-app/pkg/commandHandler/commandHnadler.go
@@ -0,0 +1,39 @@
+package commandhandler
+
+func AddTask(task string) {
+	if len(task) != 0 {
+
+	}
+}
+
+func UpdateTask(taskId int, newTask string) {
+
+}
+
+func DeleteTask(taskId int) {
+
+}
+
+func ListTasks() {
+
+}
+
+func ListDoneTasks() {
+
+}
+
+func ListTodoTasks() {
+
+}
+
+func ListInProgressTasks() {
+
+}
+
+func MarkTaskInProgress(taskId int) {
+
+}
+
+func MarkTaskDone(taskId int) {
+
+}
