2985754f105810901e87e02e5937ba7bdc0986b4
Rodion Mustafev <rodion9116@yandex.ru>
2025-01-29 15:05:53 +1000

    Added command recognition

--- my-golang-app/cmd/main.go
+++ my-golang-app/cmd/main.go
@@ -6,7 +6,36 @@ import (
 )
 
 func main() {
-	args := os.Args[1:]
+	command := os.Args[1]
 
-	fmt.Println(args)
+	switch command {
+	case "add":
+		fmt.Println("Add command")
+
+	case "update":
+		fmt.Println("Update command")
+
+	case "delete":
+		fmt.Println("delete command")
+
+	case "list":
+		if os.Args[2] == "done" {
+			fmt.Println("list done")
+		} else if os.Args[2] == "todo" {
+			fmt.Println("List todo")
+		} else if os.Args[2] == "in-progress" {
+			fmt.Println("List in-progress")
+		} else {
+			fmt.Println("Wrong command. Check tutotial")
+		}
+
+	case "mark-in-progress":
+		fmt.Println("Mark-in-progress command")
+
+	case "mark-done":
+		fmt.Println("Mark-done command")
+
+	default:
+		fmt.Println("Wrong command")
+	}
 }
