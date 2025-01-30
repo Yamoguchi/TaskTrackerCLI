a9b08ab4cc53c84449e0012b1a7908458e5710ac
Rodion Mustafev <rodion9116@yandex.ru>
2025-01-30 17:22:33 +1000

    adding commas

--- my-golang-app/internal/utils/jsonReader/jsonReader.go
+++ my-golang-app/internal/utils/jsonReader/jsonReader.go
@@ -13,6 +13,7 @@ const pathToData string = "./data/tasks.json"
 func ReadJson() ([]task.Task, error) {
 	file, err := os.Open(pathToData)
 
+	// Checking that a file exists and can be opened
 	if err != nil {
 		fmt.Println("Ошибка чтения файла")
 		return nil, err
@@ -20,19 +21,19 @@ func ReadJson() ([]task.Task, error) {
 
 	defer file.Close()
 
+	// Checking that a file is not empty
 	isEmpty, err := isJsonFileEmpty(file)
 	if err != nil {
 		return nil, err
 	}
-
 	if isEmpty {
 		return nil, fmt.Errorf("json файл пуст")
 	}
 
 	var tasks []task.Task
 
+	// Deserializing json file
 	decoder := json.NewDecoder(file)
-
 	if err := decoder.Decode(&tasks); err != nil {
 		fmt.Println("Ошибка десериализации json файла")
 		return nil, err
@@ -44,12 +45,12 @@ func ReadJson() ([]task.Task, error) {
 func isJsonFileEmpty(file *os.File) (bool, error) {
 	content, err := io.ReadAll(file)
 	if err != nil {
-		return true, err
+		return false, err
 	}
 
 	_, err = file.Seek(0, io.SeekStart)
 	if err != nil {
-		return true, err
+		return false, err
 	}
 
 	return string(content) == "{}", nil
