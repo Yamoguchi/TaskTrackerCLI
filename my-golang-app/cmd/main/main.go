fef8ffd1e3e617ff2b1f9fd86b5821b4e6fff73c
Rodion Mustafev <rodion9116@yandex.ru>
2025-01-30 21:43:20 +1000

    refactor main

--- my-golang-app/cmd/main/main.go
+++ my-golang-app/cmd/main/main.go
@@ -33,11 +33,9 @@ func main() {
 func invokeCommand(command string) {
 	switch command {
 	case "add":
-		fmt.Println("Add command")
 		Command.AddTask(os.Args[2])
 
 	case "update":
-		fmt.Println("Update command")
 		taskId, err := strconv.Atoi((os.Args[2]))
 
 		if err == nil {
@@ -60,24 +58,19 @@ func invokeCommand(command string) {
 	case "list":
 		if !(len(os.Args) < 3) {
 			if os.Args[2] == "done" {
-				fmt.Println("list done")
 				Command.ListDoneTasks()
 			} else if os.Args[2] == "todo" {
-				fmt.Println("List todo")
 				Command.ListTodoTasks()
 			} else if os.Args[2] == "in-progress" {
-				fmt.Println("List in-progress")
 				Command.ListInProgressTasks()
 			} else {
 				fmt.Println("Wrong command. Check tutotial")
 			}
 		} else {
-			fmt.Println("List command")
 			Command.ListTasks()
 		}
 
 	case "mark-in-progress":
-		fmt.Println("Mark-in-progress command")
 		taskId, err := strconv.Atoi((os.Args[2]))
 
 		if err == nil {
@@ -87,7 +80,6 @@ func invokeCommand(command string) {
 		}
 
 	case "mark-done":
-		fmt.Println("Mark-done command")
 		taskId, err := strconv.Atoi((os.Args[2]))
 
 		if err == nil {
